0663eb75290e1fe1f969ab228e651e6605ee349a
al3xkras <alexandr.krasovskiy@gmail.com>
2022-09-28 15:08:27 +0200

    UserDTO: validation constraints

--- WebNotebookAPI-commons/src/main/java/com/al3xkras/web_notebook_api/user_service/dto/UserDTO.java
+++ WebNotebookAPI-commons/src/main/java/com/al3xkras/web_notebook_api/user_service/dto/UserDTO.java
@@ -3,10 +3,22 @@ package com.al3xkras.web_notebook_api.user_service.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 public class UserDTO {
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Pattern(regexp = "[a-zA-Z0-9_]{1,15}")
     private String username;
+    @NotNull
+    @Size(min = 8, max = 150)
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-+_!@#$%^&*.,?]).{8,}$")
     private String password;
+    @Email
     private String email;
 }
